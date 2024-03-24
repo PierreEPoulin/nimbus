bcb88420375622e09e9e5ce7d6424c864bcbc0c7
Michael Wells <info@sygnal.com>
2024-03-24 06:26:15 +0000

    v0.1.6

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { RouteDispatcher } from "./routeDispatcher";
 
 // Global vars
 const SITE_NAME = 'Nimbus';
-const VERSION = 'v0.1.5';
+const VERSION = 'v0.1.6';
 
 // Global object
 window[SITE_NAME] = window[SITE_NAME] || {}; 
